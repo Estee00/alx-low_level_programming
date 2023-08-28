acd4781a825fa1796828dafe96856d46607c03cd
Estee00 <estherani10000@gmail.com>
2023-08-28 16:12:19 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: always 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *dest;
+	int n;
+	int b;
+	char s;
+	
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
